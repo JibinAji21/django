0b60c89eadf378b482d91fc98a2a162b8d4e66d5
JibinAji21 <jibinaji26@gamil.com>
2024-10-22 13:50:36 +0530

    django

---
+++ project/app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .import views
+
+
+urlpatterns=[
+    path('index',views.index_page),
+    path('a',views.fun1),
+    path('b',views.fun2),
+    path('fun3/<a>/<b>',views.fun3),
+    path('fun4/<int:a>',views.fun4),
+    path('fun5/<int:a>/<int:b>/<int:c>',views.fun5),
+    path('demo',views.demo),
+    path('snd',views.snd),
+    path('fun6',views.fun6),
+    path('edit/<id>',views.edit),
+    path('delete/<id>',views.delete),
+]
---
+++ project/app/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+todo=[{'id':'1','task':'task1'},{'id':'2','task':'task2'},{'id':'3','task':'task3'}]
+
+# Create your views here.
+def fun1(request):
+    return HttpResponse("welcome")
+
+def fun2(request):
+    return HttpResponse("hello")
+def fun3(req,a,b):
+    return HttpResponse(a)
+def fun4(req,a):
+    print(type(a))
+    return HttpResponse(a)
+def fun5(req,a,b,c):
+    if a>b and a>c:
+        return HttpResponse(a)
+    elif b>a and b>c:
+        return  HttpResponse(b)
+    else:
+        return HttpResponse(c)
+
+def index_page(req):
+    name="Akhil"
+    age=20
+    place='ekm'
+    return render(req,'index.html',{'name':name,'age':age,"place":place})
+
+def demo(req):
+    l=[{'name':'sam','age':21},{'name':'sony','age':21},{'name':'iqbal','age':28}]
+    d={'name':'shane','age':23}
+    return render(req,"demo.html",{"data":l,"data1":d})
+
+def snd(req):
+    return render(req,"second.html")
+
+def fun6(req):
+    if req.method=='POST':
+        id=req.POST['id']
+        task=req.POST['task']
+        todo.append({'id':id,'task':task})
+        print(todo)
+        return redirect(fun6)
+    return render(req,'tododisplay.html',{'todo':todo})
+
+def edit(req,id):
+    task=''
+    for i in todo:
+        if i['id']==id:
+            task=i
+    if req.method=='POST':
+        id=req.POST['id']
+        task1=req.POST['task']
+        task['id']=id
+        task['task']=task1
+        return redirect(fun6)
+    return render(req,'edit.html',{'task':task})
+
+def delete(req,id):
+    for i in todo:
+        if i['id']==id:
+            todo.remove(i)
+    return redirect(fun6)
+
+
+
+
+
