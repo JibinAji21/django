86f0e960765413e7d47c8a5486c53a4d94bea8b2
JibinAji21 <jibinaji26@gamil.com>
2024-10-25 13:48:16 +0530

    django template edit

--- stdproject/app1/urls.py
+++ stdproject/app1/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path('disp_std',views.disp_std),
     path('add_std',views.add_std),
     path('edit_std/<id>',views.edit_std),
+    path('delete_std/<id>',views.delete_std),
   
 ]
